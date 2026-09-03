775bdd92184d47002b81b052f6ccc0dc6ce772b8
Jeremie <jeremie@kwapong.com>
2026-09-02 22:46:56 -0400

    Update gsg.py

--- Nerual Network Test/gsg.py
+++ Nerual Network Test/gsg.py
@@ -7,4 +7,11 @@ class NeuralNetwork():
         np.random.seed(1)
         self.synaptic_weights = 2*np.random.random((3,1))-1
 
+    def sigmold(self,x):
+          return 1/ (1* np.exp(-x))
   
+      def sigmold_deritavtive(self, x):
+          return x * (1-x)
+  
+     
+     
